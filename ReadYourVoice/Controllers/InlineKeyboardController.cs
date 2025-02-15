b8e1ca8075e6da1018944d651d0b3dcef769c68c
Name <email@example.ru>
2025-02-15 17:01:59 +0300

    Update class InlineKeyboardController

--- ReadYourVoice/Controllers/InlineKeyboardController.cs
+++ ReadYourVoice/Controllers/InlineKeyboardController.cs
@@ -1,20 +1,46 @@
-﻿using Telegram.Bot;
+﻿using ReadYourVoice.Services;
+using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace ReadYourVoice.Controllers;
 
 class InlineKeyboardController
 {
     private readonly ITelegramBotClient _telegramBotClient;
+    private readonly IStorage _memoryStorage;
 
-    public InlineKeyboardController(ITelegramBotClient telegramBotClient)
+    public InlineKeyboardController(ITelegramBotClient telegramBotClient, IStorage memoryStorage)
     {
         _telegramBotClient = telegramBotClient;
+        _memoryStorage = memoryStorage;
     }
 
     public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
     {
-        Console.WriteLine($"Контроллер {GetType().Name} обнаружил нажатие на кнопку");
+        if (callbackQuery?.Data == null)
+        {
+            return;
+        }
+
+        // Обновление пользовательской сессии новыми данными
+        _memoryStorage.GetSession(callbackQuery.From.Id).LanguageCode = callbackQuery.Data;
+
+        // Генерим информационное сообщение
+        string languageText = callbackQuery.Data switch
+        {
+            "ru" => " Русский",
+            "en" => " Английский",
+            _ => String.Empty
+        };
+
+        // Отправляем в ответ уведомление о выборе
+        await _telegramBotClient.SendTextMessageAsync(callbackQuery.From.Id,
+            $"<b>Язык аудио - {languageText}.{Environment.NewLine}</b>" +
+            $"{Environment.NewLine}Можно поменять в главном меню.", cancellationToken: ct,
+            parseMode: ParseMode.Html);
+
+        Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
 
         await _telegramBotClient.SendTextMessageAsync(callbackQuery.From.Id, $"Обнаружено нажатие на кнопку", cancellationToken: ct);
     }
